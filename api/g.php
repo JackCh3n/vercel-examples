febad89a34870b4f680c63948edc6cf8b6e79833
jackChen <1272016935@qq.com>
2022-06-24 15:01:13 +0800

    w3

--- api/g.php
+++ api/g.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($_GET['f'])) {
-    exit(file_get_contents('/tmp/'.$_GET['f'].'.json'));
+    var_dump(file_get_contents('/tmp/'.$_GET['f'].'.json'));
+    var_dump(file_get_contents('./tmp/'.$_GET['f'].'.json'));
 }else{
     var_dump(scandir('/tmp'));
 }
