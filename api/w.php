421dd40711536b7e2f07b9db2af6ed49f1f682aa
jackChen <1272016935@qq.com>
2022-06-24 14:41:21 +0800

    w2

--- api/w.php
+++ api/w.php
@@ -1,8 +1,8 @@
 <?php
 $a = [
-    't':time(),
-    'f':md5(time())
-]
+    't'=>time(),
+    'f'=>md5(time())
+];
 var_dump($a);
 file_put_contents('/tmp/'.$a['f'].'.json', json_encode($a));
 var_dump($a);
