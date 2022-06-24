5e30afdd27a4832d64f491b85ff73fc4850d6b76
jackChen <1272016935@qq.com>
2022-06-24 14:28:02 +0800

    dir

--- api/w.php
+++ api/w.php
@@ -3,5 +3,5 @@ $a = [
     't':time(),
     'f':md5(time())
 ]
-file_put_contents('/tmp'.$a['f'].'.json', json_encode($a));
+file_put_contents('/tmp/'.$a['f'].'.json', json_encode($a));
 exit(json_encode($a));
