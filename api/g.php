82efd8cc2b2a110522416ff47763aa3def537de2
jackChen <1272016935@qq.com>
2022-06-24 14:24:31 +0800

    get write

---
+++ api/g.php
@@ -0,0 +1,6 @@
+<?php
+if (isset($_GET['f'])) {
+    exit(file_get_contents('/tmp'.$_GET['f'].'.json'));
+}else{
+    var_dump(scandir('/tmp'));
+}
---
+++ api/w.php
@@ -0,0 +1,7 @@
+<?php
+$a = [
+    't':time(),
+    'f':md5(time())
+]
+file_put_contents('/tmp'.$a['f'].'.json', json_encode($a));
+exit(json_encode($a));
